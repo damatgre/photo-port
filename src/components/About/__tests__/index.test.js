93d517be079cbf777bbfbab4daa81e700db51a5b
damatgre <gregdamato2@gmail.com>
2021-08-22 16:41:46 -0400

    first tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //verify that the component is rendering
+    it('renders', () => {
+        render(<About />);
+    });
+
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />)
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
